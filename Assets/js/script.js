dbc3bde32febf3afc26d24df7b21b4f7603899d4
Penguuu <pengvang95@yahoo.com>
2023-04-10 22:47:06 -0500

    hour function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,15 +4,25 @@ var today = dayjs().format('MMMM D YYYY, h:mm a');
 $("#currentDay").text(today);
 
 
-// saves data with save button 
-$(document).ready(function () {
+    var currentHour = dayjs().hour();
+  
+      if (currentHour < today) {
+        $(this).addClass("past");
+      } else if (currentHour === today) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+
+
+    $(document).ready(function () {
 
     $(".saveBtn").on("click", function () {
         var textValue = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, textValue);
         alert("Data saved");
-    });
+    })
 
     // local storage for each time
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
@@ -24,4 +34,8 @@ $(document).ready(function () {
     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+    
 });
+
